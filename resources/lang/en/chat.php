3d9482cbfd6ce0bbe1e1e301133a1ad2c7043430
josecortese <jcortesebustos@gmail.com>
2021-01-19 19:45:31 -0300

    First Commit

---
+++ resources/lang/en/chat.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+    'live_chat' => 'Live Chat',
+    'chat' => 'Chat',
+    'chats' => 'Chats',
+    'chat_room' => 'Chat Room',
+    'chat_group' => 'Chat Group',
+    'chat_group_name' => 'Name of Chat Group',
+    'message' => 'Message',
+    'messages' => 'Messages',
+    'member' => 'Member',
+    'members' => 'Members',
+    'props' => array(
+        'body' => 'Body'
+    ),
+    'type_a_message' => 'Type a message...',
+    'no_messages_yet' => 'No messages yet!',
+    'no_chats_yet' => 'No chats yet!',
+    'no_more_messages' => 'No more messages!',
+    'no_result_found' => 'No result found!',
+    'only_you_are_member' => 'Only you are member!',
+    'and_n_other_members' => ' & :attribute other members',
+);
